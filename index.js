0812041bf68bef49953fba4fb3ed8685313ce0e9
kaan <kabuke13@gmail.com>
2023-08-19 17:28:17 +0100

    lab changes

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded",() => test())
+function test(){
+    const text = document.getElementById("text")
+    text.textContent = "This is really cool!"
+}
